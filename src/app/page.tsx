f6f654c7ea4863128cf26bfbde9478b141f80d7a
Shweta <beherashweta54@gmail.com>
2025-03-22 06:13:43 +0530

    added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Forumactivity from "@/components/Forumactivity";
 import Hero from "../components/Hero";
 import Homecards from "../components/Homecards";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero/>
       <Homecards darkMode={false}/>
       <Forumactivity/>
+      <Footer/>
     </main>
     </>
   );
